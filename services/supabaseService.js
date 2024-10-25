5359fc55a248d36b2790a836d509365c8c4a194e
KaykyOne <KaykyOne@users.noreply.github.com>
2024-10-25 00:30:27 +0000

    deploy: bfd456bb159369561a3d0a1ce258a522b5f3421a

--- services/supabaseService.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { URL, KEY} from '@env';
-
-const supabaseUrl = URL;
-const supabaseKey = KEY;
-export const supabase = createClient(supabaseUrl, supabaseKey);
